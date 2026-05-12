02200ce4c79172824ce8fcde6677cbbd3bcd3ee7
sutasu272 <ost1902@gmail.com>
2026-05-12 16:22:54 +0500

    first commit

--- main.go
+++ main.go
@@ -2,64 +2,55 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	// Задание 1
-	bannerWidth := 12
-	bannerHeight := 8
-	bannerArea := bannerWidth * bannerHeight
-	fmt.Println(bannerArea)
-	halfBannerArea := bannerArea / 2
-	fmt.Println(halfBannerArea)
-	bannerBorderLength := (bannerWidth + bannerHeight) * 2
-	fmt.Println(bannerBorderLength)
-	// Задание 2
-	boxCount := 29
-	leftoverBoxes := boxCount % 5
-	fmt.Println(leftoverBoxes)
-	// Задание 3
-	tempMorning := 15
-	tempAfternoon := 22
-	tempEvening := 17
-	totalTemp := tempMorning + tempAfternoon + tempEvening
-	averageTemp := totalTemp / 3
-	fmt.Println(averageTemp)
-	// Задание 4
-	knownWords := 48
-	wordsGoal := 120
-	progressPercent := (float64(knownWords) / float64(wordsGoal)) * 100
-	fmt.Println(progressPercent, "%")
-	// Задание 5
-	coins := 0
-	coins += 500
-	fmt.Println(coins)
-	coins += 1200
-	fmt.Println(coins)
-	coins /= 2
-	fmt.Println(coins)
-	coins *= 2
-	fmt.Println(coins)
-	coins -= 300
-	fmt.Println(coins)
-	// Задание 6
-	participants := 42
-	groupCount := 8
-	participantsPerGroup := participants / groupCount
-	fmt.Println(participantsPerGroup)
-	// Задание 7
-	fmt.Println(20 - 4*3)
-	fmt.Println((20 - 4) * 3)
-	// результаты отличаются из-за порядка выполнения операций. В первом случае сначала выполняется умножение, а затем вычитание,
-	//что дает результат 8. Во втором случае сначала выполняется вычитание, а затем умножение, что дает результат 48.
+	const (
+		BaseTariff    = 0.45 // цена за 1 кВт·ч
+		HighLoadTax   = 0.15 // налог на высокое потребление
+		NightDiscount = 0.30 // ночная скидка
+	)
+	for {
+		var name string
+		var W int
+		var hours int
+		var night bool
+		fmt.Print("Введите название прибора (или 'done'):")
+		fmt.Scanln(&name)
+		fmt.Print("Мощность(Вт):")
+		fmt.Scanln(&W)
+		fmt.Print("Время работы(часы):")
+		fmt.Scanln(&hours)
+		fmt.Print("Ночной режим (true/false):")
+		fmt.Scanln(&night)
+		// Расход
+		consumption := float64(W) * float64(hours) / 1000.0 // кВт·ч
+		// Стоимость
+		cost := consumption * BaseTariff
+		if night == true {
+			cost *= float64(NightDiscount)
+		}
+		if consumption > 10 {
+			cost += float64(HighLoadTax) * cost
+		}
+		category := ""
+		switch {
+		case W < 100:
+			category = "Экономный"
+		case W >= 100 && W < 1000:
+			category = "Стандартный"
+		case W >= 1000:
+			category = "Мощный"
+		}
+		fmt.Println()
+		fmt.Printf("Прибор: %s [Категория: %s]\n", name, category)
+		fmt.Printf("Расход: %.2f кВт·ч\n", consumption)
+		fmt.Printf("Стоимость: %.2f .\n", cost)
+		fmt.Println()
 
-	// Задание 8
-	squareValue := 81
-	result := math.Sqrt(float64(squareValue))
-	multiplier := 5
-	exponent := 2
-	result2 := int(math.Pow(float64(multiplier), float64(exponent)))
-	fmt.Println(result)
-	fmt.Println(result2)
+		if name == "done" {
+			break
+		}
+	}
+	fmt.Println("расчет завершен.")
 }
